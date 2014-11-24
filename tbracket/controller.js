f314a1c6d54d774e9ecefecd40ad0f9d84e65ff0
Riley Duke <rileyduke@gmail.com>
2014-11-24 02:04:47 -0500

    init tournaments

--- tbracket/controller.js
+++ tbracket/controller.js
@@ -1,7 +1,9 @@
 tbracketApp.controller('tb-controller', ['$scope', function($scope) {
 
-  var counter = 0;
-
+  //see notes for variables
+  $scope.rounds = [];
+  $scope.tournamentSize = 0;
+  $scope.roundCount = 0;
   $scope.players = [
     {
       display: 'Peno',
@@ -17,12 +19,76 @@ tbracketApp.controller('tb-controller', ['$scope', function($scope) {
     }
   ];
 
+  //start with 0 players
+  var playerCount = $scope.players.length;
+
+  //adds player to the players list
   $scope.addPlayer = function(name,display){
-    counter++;
+    playerCount++;
     $scope.players.push({
-      name: (name) ? name : 'New Player ' + counter,
-      display: (display) ? display : 'New Display ' + counter
+      name: (name) ? name : 'New Player ' + playerCount,
+      display: (display) ? display : 'New Display ' + playerCount
     });
+    
+    //reset global binds
+    $scope.inputDisplay = '';
+    $scope.inputName = '';
+
+    //log test
+    // $scope.players.forEach(function(element,index,array){
+    //   console.log('a[' + index + '] = ' + element.name);
+    // });
   };
+
+  //function to calculate tournament size
+  //the size of the tournament is the next power of 2 after the player count
+  //  eg 54 players = 64 player tournament (10 people get a by)
+  //the number of rounds is the power of 2 divided by 2
+  $scope.calculateTournamentSize = function(){
+    $scope.roundCount = Math.ceil(Math.log2(playerCount));
+    $scope.tournamentSize = Math.pow(2,$scope.roundCount);
+    console.log($scope.tournamentSize);
+  }
+
+  //create a list of rounds
+  //the list of rounds is the size of the calculated roundCount
+  $scope.initRounds = function(){
+    for (i = 0; i < $scope.roundCount; i++) {
+      $scope.rounds.push({
+        roundNumber : i + 1,
+        roundMatchCount : Math.pow(2,$scope.roundCount - i)/2
+      });
+    }
+    //log test
+    // $scope.rounds.forEach(function(element,index,array){
+    //   console.log(element.roundNumber + ': ' + element.roundMatchCount);
+    // });
+  }
+
+  //create list of matches for each round
+  $scope.initMatches = function(){
+    $scope.rounds.forEach(function(element,index,array){
+      element.matches = [];
+      for (var i = 0; i < element.roundMatchCount; i++) {
+        element.matches.push({
+          round: element.roundNumber,
+          match: i+1
+        });
+      };
+    });
+    //log test
+    // $scope.rounds.forEach(function(element,index,array){
+    //   element.matches.forEach(function(element,index,array){
+    //     console.log(element.round + ', ' + element.match);
+    //   });
+    // });
+  }
+
+  //do all the necessary stuff to start the tournament
+  $scope.initTournament = function(){
+    $scope.calculateTournamentSize();
+    $scope.initRounds();
+    $scope.initMatches();
+  }
 
 }]);
